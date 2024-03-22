013c775a8100427e621a815828369ca44336736d
Liuuwei <6iu5ei@gmail.com>
2024-03-23 02:58:52 +0800

    change keyToChar

--- include/Tools.h
+++ include/Tools.h
@@ -3,6 +3,7 @@
 #include "GLFW/glfw3.h"
 #include "vulkan/vulkan_core.h"
 #include <cmath>
+#include <cstddef>
 #include <glm/glm.hpp>
 #include <algorithm>
 #include <limits>
@@ -319,62 +320,33 @@ static float pointToLineLength(float a, float b, float x, float y) {
 }
 
 static char keyToChar(int key) {
-    switch (key) {
-    case GLFW_KEY_A:
-        return 'a';
-    case GLFW_KEY_B:
-        return 'b';
-    case GLFW_KEY_C:
-        return 'c';
-    case GLFW_KEY_D:
-        return 'd';
-    case GLFW_KEY_E:
-        return 'e';
-    case GLFW_KEY_F:
-        return 'f';
-    case GLFW_KEY_G:
-        return 'g';
-    case GLFW_KEY_H:
-        return 'h';
-    case GLFW_KEY_I:
-        return 'i';
-    case GLFW_KEY_J:
-        return 'j';
-    case GLFW_KEY_K:
-        return 'k';
-    case GLFW_KEY_L:
-        return 'l';
-    case GLFW_KEY_M:
-        return 'm';
-    case GLFW_KEY_N:
-        return 'n';
-    case GLFW_KEY_O:
-        return 'o';
-    case GLFW_KEY_P:
-        return 'p';
-    case GLFW_KEY_Q:
-        return 'q';
-    case GLFW_KEY_R:
-        return 'r';
-    case GLFW_KEY_S:
-        return 's';
-    case GLFW_KEY_T:
-        return 't';
-    case GLFW_KEY_U:
-        return 'u';
-    case GLFW_KEY_V:
-        return 'v';
-    case GLFW_KEY_W:
-        return 'w';
-    case GLFW_KEY_X:
-        return 'x';
-    case GLFW_KEY_Y:
-        return 'y';
-    case GLFW_KEY_Z:
-        return 'z';
+    return static_cast<char>(key);
+
+    return '?';
+}
+
+static std::string rmFrontSpace(const std::string& str) {
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        if (*it != ' ') {
+            return {it, str.end()};
+        }
     }
 
-    return ' ';
+    return {};
+}
+
+static std::string rmBackSpace(const std::string& str) {
+    for (auto it = str.rbegin(); it != str.rend(); ++it) {
+        if (*it != ' ') {
+            return {str.begin(), it.base()};
+        }
+    }
+
+    return {};
+}
+
+static std::string rmSpace(const std::string& str) {
+    return rmBackSpace(rmFrontSpace(str));
 }
 
 };
