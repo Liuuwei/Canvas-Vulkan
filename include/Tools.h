b6874abeee0baa10e9b30c79a616c67d97b0fdde
Liuuwei <6iu5ei@gmail.com>
2024-03-23 22:44:36 +0800

    add charToShiftChar

--- include/Tools.h
+++ include/Tools.h
@@ -325,6 +325,55 @@ static char keyToChar(int key) {
     return '?';
 }
 
+static char charToShiftChar(char c) {
+    switch (c) {
+        case '`':
+            return '~';
+        case '1': 
+            return '!';
+        case '2':
+            return '@';
+        case '3':
+            return '#';
+        case '4':
+            return '$';
+        case '5': 
+            return '%';
+        case '6':
+            return '^';
+        case '7':
+            return '&';
+        case '8':
+            return '*';
+        case '9':
+            return '(';
+        case '0':
+            return ')';
+        case '-':
+            return '_';
+        case '=':
+            return '+'; 
+        case '[':
+            return '{';
+        case ']':
+            return '}';
+        case '\\':
+            return '|';
+        case ';':
+            return ':';
+        case '\'':
+            return '\"';
+        case ',':
+            return '<';
+        case '.':
+            return '>';
+        case '/':
+            return '?';
+    }
+
+    return c;
+}
+
 static std::string rmFrontSpace(const std::string& str) {
     for (auto it = str.begin(); it != str.end(); ++it) {
         if (*it != ' ') {
